c26575e977c2e73c627839a49479a22d34e81b51
Zayaan <zayaan6k5029@gmail.com>
2026-08-22 04:58:25 +0530

    Increased number of saved generations to 15

--- modules/nix-cleanup.nix
+++ modules/nix-cleanup.nix
@@ -2,7 +2,7 @@
 
 {
   # Limit boot generations strictly to 10 on every rebuild
-  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.systemd-boot.configurationLimit = 15;
 
   # Automatically clean up orphaned packages from deleted generations
   nix.gc = {
