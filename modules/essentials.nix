5bc1b8db218a07189f6c95ace810bc818a8ac393
Zayaan <zayaan6k5029@gmail.com>
2026-09-05 21:01:16 +0530

    Made changes to tlp

--- modules/essentials.nix
+++ modules/essentials.nix
@@ -10,6 +10,22 @@
   services.tlp = {                   # Power Management Daemon
     enable = true;
     pd.enable = true;
+
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+
+      # TLP_DEFAULT_MODE = "SAV";
+      # TLP_PERSISTENT_DEFAULT = 1;
+
+      CPU_BOOST_ON_BAT = 0;
+      CPU_HWP_DYN_BOOST_ON_BAT = 0;
+    };
   };
   programs.xwayland.enable = true;   # Enables X11 support on Walyand
   hardware.uinput.enable = true;     # Enables applications to create virtual input devices
